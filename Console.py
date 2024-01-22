489767ae1ba5e246a2dfca386e24da9713d2d457
amitkrec@gmail.com <amitkrec@gmail.com>
2024-01-22 11:21:15 +0530

    Display any string

--- Console.py
+++ Console.py
@@ -1,1 +1,9 @@
-print("My Name is Amit Pal..")
+# Hi Amit,see you later...
+'''s=input("Amit")
+print(f"Hi {s}, see you later..")
+'''
+def greet():           
+    name = input("What is your name")
+    res=print(f"Hi {name}, see you later..")
+    return res
+greet()
