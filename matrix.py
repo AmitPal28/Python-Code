8298872626c186da6db92010bc602ee7999908f5
amitkrec@gmail.com <amitkrec@gmail.com>
2024-01-27 12:10:58 +0530

    Sum of maximum number in a metrix

--- matrix.py
+++ matrix.py
@@ -1,4 +1,4 @@
-
+'''
 #Find the sum of minimum value of each rows in a metrix.
 def find_min_sum(matrix):
     min_sum = 0
@@ -18,3 +18,27 @@ for i in range(M):
 # Output
 result = find_min_sum(matrix)
 print(result)
+'''
+
+
+'''
+#Find the sum of maximum value of each rows in a metrix.
+def find_max_sum(matrix):
+    max_sum = 0
+    for row in matrix:
+        max_value = max(row)
+        max_sum += min_value
+    return max_sum
+
+# Input
+m, n = map(int, input().split())
+matrix = []
+
+for i in range(m):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+# Output
+result = find_max_sum(matrix)
+print(result)
+'''
